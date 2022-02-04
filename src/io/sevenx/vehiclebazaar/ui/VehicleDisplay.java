98e0fe9ab8b0e5c925ed8da2075bcf0d74227dac
neelamvijayvargiya <98867683+neelamvijayvargiya@users.noreply.github.com>
2022-02-04 13:19:40 +0530

    Update VehicleDisplay.java

--- src/io/sevenx/vehiclebazaar/ui/VehicleDisplay.java
+++ src/io/sevenx/vehiclebazaar/ui/VehicleDisplay.java
@@ -1,9 +1,55 @@
-package io.sevenx.vehiclebazaar.ui;
+package io.sevenx.vehiclebazaar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.sevenx.vehiclebazaar.data.Bike;
 import io.sevenx.vehiclebazaar.data.Car;
+import io.sevenx.vehiclebazaar.data.Vehicle;
 
-public class VehicleDisplay {
+
+public class VehicleBazaarMain {
 	
-	public void display(Car car) {
-		System.out.println("Display Car Information");	}
-}
+
+		public static void main(String args[]) {
+			Vehicle bike=new Bike();
+			bike.setBrand("Tata Bazaz");
+			bike.setModel("Duke");
+			bike.setFuelType("Petrol");
+			bike.setMaximumSpeed(110);
+			bike.setColor("Orange");
+			
+			Vehicle car=new Car();
+			car.setBrand("Tata");
+			car.setModel("Harrier");
+			car.setFuelType("Diesel");
+			car.setMaximumSpeed(180);
+			car.setColor("Grey");
+			
+			List <Vehicle> vehicle=new ArrayList <Vehicle>();
+			vehicle.add(bike);
+			vehicle.add(car);
+			for(Vehicle v:vehicle) {
+				System.out.println("brand is: "+v.getBrand());
+				System.out.println("model is: "+v.getModel());
+				System.out.println("fuel type is: "+v.getFuelType());
+				System.out.println("maximum speed is: "+v.getMaximumSpeed()+"kmph");
+				System.out.println("color is: "+v.getColor());
+				if (v instanceof Bike) {
+					Bike bike1=(Bike)v;
+					bike1.setPower(8.36);
+					bike1.setNumberOfGears(5);
+					System.out.println("power is: "+bike1.getPower()+"PS");
+					System.out.println("number of gears are: "+bike1.getNumberOfGears()+"\n");
+				}
+				else if (v instanceof Car) {
+					Car car1=(Car)v;
+					car1.setBreakhorsePower(125);
+					car1.setTransmissionType("Automatic");
+					System.out.println("horse power is: "+car1.getBreakhorsePower()+"kW");
+					System.out.println("transmission type is: "+car1.getTransmissionType()+"\n");
+					
+				}
+			}
+			}
+		}

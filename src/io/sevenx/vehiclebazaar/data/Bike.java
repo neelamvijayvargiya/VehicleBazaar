0330d5673eb5b78c937e517a768b38fea7ef115a
neelamvijayvargiya <98867683+neelamvijayvargiya@users.noreply.github.com>
2022-02-04 13:22:40 +0530

    Update Bike.java

--- src/io/sevenx/vehiclebazaar/data/Bike.java
+++ src/io/sevenx/vehiclebazaar/data/Bike.java
@@ -1,16 +1,35 @@
 package io.sevenx.vehiclebazaar.data;
 
-public class Bike extends Vehicle {
-	
-	public int getPower;
-
-	public int getPower() {
+public class Bike extends Vehicle{
+	private double power;
+	private int numberOfGears;
+	private String tyreType;
+	boolean discBreak;
+	public double getPower() {
 		return power;
 	}
-
-	public void setPower(int power) {
+	public void setPower(double power) {
 		this.power = power;
+	}
+	public int getNumberOfGears() {
+		return numberOfGears;
+	}
+	public void setNumberOfGears(int numberOfGears) {
+		this.numberOfGears = numberOfGears;
+	}
+	public String getTyreType() {
+		return tyreType;
+	}
+	public void setTyreType(String tyreType) {
+		this.tyreType = tyreType;
+	}
+	public boolean isDiscBreak() {
+		return discBreak;
+	}
+	public void setDiscBreak(boolean discBreak) {
+		this.discBreak = discBreak;
 		
 	}
-	private int power;
 }
+	
+	

300697ba40004b2a912513a099370ea0aba1708c
neelamvijayvargiya <98867683+neelamvijayvargiya@users.noreply.github.com>
2022-02-04 13:21:52 +0530

    Update Car.java

--- src/io/sevenx/vehiclebazaar/data/Car.java
+++ src/io/sevenx/vehiclebazaar/data/Car.java
@@ -1,15 +1,36 @@
 package io.sevenx.vehiclebazaar.data;
 
 public class Car extends Vehicle {
-
-	public int horsePower;
-
-	public int gethorsePower() {
-		return horsePower;
-	}
-
-	public void sethorsePower( int horsePower) {
-		this.horsePower = horsePower;
+	
+		private double breakhorsePower;
+		private String transmissionType;
+		boolean powerSteering;
+		private int numberOfSeats;
 		
-	}
+		public double getBreakhorsePower() {
+			return breakhorsePower;
+		}
+		public void setBreakhorsePower(double breakhorsePower) {
+			this.breakhorsePower = breakhorsePower;
+		}
+		public String getTransmissionType() {
+			return transmissionType;
+		}
+		public void setTransmissionType(String transmissionType) {
+			this.transmissionType = transmissionType;
+		}
+		public boolean isPowerSteering() {
+			return powerSteering;
+		}
+		public void setPowerSteering(boolean powerSteering) {
+			this.powerSteering = powerSteering;
+		}
+		public int getNumberOfSeats() {
+			return numberOfSeats;
+		}
+		public void setNumberOfSeats(int numberOfSeats) {
+			this.numberOfSeats = numberOfSeats;
+		}
+		
+		
 }
